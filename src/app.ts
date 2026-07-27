a98723fac4a9677a7eff82e58b026019a210f2a1
AxelRG07 <axelrepeat@gmail.com>
2026-07-27 12:33:53 -0600

    Practica #3 - Transacciones endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import fs from 'fs';
 
 const app = express();
 const PORT = 3002;
+
+// Mètodo que permite al servidor entender el JSON que se envía en el cuerpo de la solicitud
+app.use(express.json());
+
+interface User {
+  id: number;
+  password?: string;
+  first_name?: string;
+  last_name?: string;
+  username?: string;
+  email?: string;
+  gender?: string;
+}
 
   //-------------------get-------------------------------------------
 
@@ -19,8 +33,21 @@ const PORT = 3002;
 //   res.send('Hello World!!');
 // });
 
+// Modulo fs(File System) para leer y escribir archivos locales
 app.get('/users', (req: Request, res: Response) => {
-  res.send('Petición GET a /users');
+  res.send(fs.readFileSync('./data/users.json', 'utf8'));
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  const user = users.find((user: any) => user.id === id);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+  return 
 });
 
 //---------------------------------post-------------------------------
@@ -31,7 +58,14 @@ app.get('/users', (req: Request, res: Response) => {
 // });
 
 app.post('/users', (req: Request, res: Response) => {
-  res.send('Petición POST a /users');
+  const newUser: User = req.body;
+  // Leer el archivo JSON
+  const users: User[] = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  // Agregar el nuevo usuario
+  users.push(newUser);
+  // Escribir el archivo JSON
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  res.send(`{"Usuario ${newUser.first_name} creado correctamente"}`);
 });
 
 //-------------------------------put-------------------------------
@@ -40,8 +74,22 @@ app.post('/users', (req: Request, res: Response) => {
 //   res.send('Test PUT');
 // });
 
-app.put('/users', (req: Request, res: Response) => {
-  res.send('Petición PUT a /users');
+app.put('/users/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const users: User[] = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  const userId = users.findIndex(u => u.id === id);
+  if (userId === -1) {
+    res.status(404).send('Usuario no encontrado');
+    return;
+  }
+  const updatedUser = {
+    ...users[userId],
+    ...req.body,
+    id: id,
+  };
+  users[userId] = updatedUser;
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  res.send(`Usuario ${id} actualizado correctamente`);
 });
 
 
@@ -50,8 +98,17 @@ app.put('/users', (req: Request, res: Response) => {
 //   res.send('Test DELETE');
 // });
 
-app.delete('/users', (req: Request, res: Response) => {
-  res.send('Petición DELETE a /users');
+app.delete('/users/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const users: User[] = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  const userId = users.findIndex(user => user.id === id);
+  if (userId === -1) {
+    res.status(404).send('Usuario no encontrado');
+    return;
+  }
+  users.splice(userId, 1);
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  res.send(`Usuario ${id} eliminado correctamente`);
 });
 
 app.use('all', (req: Request, res: Response) => {
